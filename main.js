bf3a35e84d3fd3c22e7dd4aa070efe384202f37f
BackstageSeeker <108864794+BackstageSeeker@users.noreply.github.com>
2022-08-20 13:42:05 +0200

    Navbar
    Hamburger menu for mobile + responsive sections

--- main.js
+++ main.js
@@ -90,12 +90,112 @@ dropdowns.forEach(dropdown => {
         })
     })
 })
-
+/*
 const boxes = document.querySelector('.boxes')
 const boxesGapPx = window.getComputedStyle(boxes, null).getPropertyValue('row-gap')
 const imgHeightPx = window.getComputedStyle(document.querySelector('#height-subject'), null).getPropertyValue('height')
 let boxesGap = parseInt(boxesGapPx.substring( 0,  boxesGapPx.length - 2))
 let imgHeight = parseInt(imgHeightPx.substring( 0,  imgHeightPx.length - 2))
-
+console.log(getComputedStyle(boxes, null))
 console.log((imgHeight*2) + boxesGap)
 boxes.style.height =  (imgHeight*2) + boxesGap + 8 + 'px'
+*/
+
+let menuOpened = false;
+
+let scrolling;
+window.addEventListener('scroll',() => {
+    scrolling = true;
+})
+scrollInterval = setInterval(scrollWatch, 300)
+function scrollWatch() {
+    if (scrolling === true) {
+        scrolling = false;
+        const scrolled = window.scrollY
+        if (menuOpened === false) {
+            console.log(menuOpened)
+            if (scrolled > 100) {
+                document.querySelector('.nav').style.backgroundColor = "rgba(0 ,0 ,0, 0.4)"
+            }
+            else {
+                document.querySelector('.nav').style.backgroundColor = "transparent"
+            }
+        }
+    }
+}
+
+
+const menu = document.getElementById("menu");
+const hamburger = document.querySelector('.hamburger-holder')
+const nav = document.querySelector('.nav')
+const navObj = {
+    get height() {
+        return nav.getBoundingClientRect().height
+    }
+}
+const iCount = document.querySelectorAll('.item').length
+const iPaddingStr = window.getComputedStyle(document.querySelector('.item')).getPropertyValue('padding-top')
+const iPadding = parseInt(iPaddingStr.substring( 0, iPaddingStr.length - 2))
+const itemHeightStr = window.getComputedStyle(document.querySelector('.item')).getPropertyValue('height')
+const itemHeight = parseInt(itemHeightStr.substring( 0, itemHeightStr.length - 2))
+const oHeight = iPadding*2*iCount
+menu.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    hamburger.classList.toggle("trans")
+    if (menuOpened === false) {
+        nav.style.backgroundColor = "rgba(0 ,0 ,0, 0.8)"
+        nav.style.minHeight = itemHeight*iCount + iPadding*2 + 64 + "px"
+        hamburger.style.maxHeight = itemHeight*iCount + "px"
+        console.log(iPadding)
+        menuOpened = true
+    }
+    else {
+        console.log("asdddd")
+        nav.style.minHeight = 64 + "px"
+        hamburger.style.maxHeight = "0px"
+        document.querySelectorAll('.mini-items').forEach(it => {
+            it.style.cssText += 'height: 0; transform: rotateX: 0deg;'
+            if (!it.className.includes('trans')) {
+                it.classList.add('trans')
+            }
+        })
+        document.querySelectorAll('.item').forEach(item => {
+            if (item.className.includes('opened')) {
+                item.classList.remove('opened')
+            }
+        })
+        menuOpened = false;
+    }
+});
+let miniOpened = 0;
+
+document.querySelectorAll('.item').forEach(item => {
+    item.addEventListener('click', () => {
+        if (item.nextElementSibling.className.includes('mini-items')) {
+            item.nextElementSibling.classList.toggle('trans')
+            item.classList.toggle('opened')
+            const miniHeight = Array.from(item.nextElementSibling.children).length * itemHeight
+            if (item.className.includes('opened')) {
+                miniOpened += 1
+                console.log(miniHeight)
+                item.nextElementSibling.style.height = miniHeight + 'px'
+                nav.style.minHeight = navObj.height + miniHeight + 'px'
+                Array.from(item.children).forEach(child => {
+                    if (child.className.includes('fa-chevron-left')) {
+                        child.classList.add('rt-90')
+                    }
+                })
+            }
+            else {
+                miniOpened -= 1
+                item.nextElementSibling.style.height = '0px'
+                nav.style.minHeight = navObj.height - miniHeight + 'px'
+                Array.from(item.children).forEach(child => {
+                    if (child.className.includes('fa-chevron-left')) {
+                        child.classList.remove('rt-90')
+                    }
+                })
+            }
+        }
+    })
+})
